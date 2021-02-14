32021771740d4c756ff7ae1b1555eac9848f9a80
goodmeow <harunbam3@gmail.com>
2021-02-14 19:41:39 +0700

    init komodo

--- komodo_beryllium.mk
+++ komodo_beryllium.mk
@@ -6,21 +6,21 @@
 
 $(call inherit-product, device/xiaomi/beryllium/device.mk)
 
-# Inherit some common nad stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
-NAD_BUILD_TYPE := OFFICIAL
+# Inherit some common komodo stuff.
+$(call inherit-product, vendor/komodo/config/common_full_phone.mk)
+KOMODO_BUILD_TYPE := OFFICIAL
+KOMODO_VARIANT ?= BETA
 TARGET_BOOT_ANIMATION_RES := 1080
-USE_GAPPS ?= true
-USE_AOSP_CLOCK := true
+KOMODO_GAPPS_TYPE := gapps
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nad_beryllium
+PRODUCT_NAME := komodo_beryllium
 PRODUCT_DEVICE := beryllium
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Pocophone F1
 PRODUCT_MANUFACTURER := Xiaomi
 
-#BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="beryllium-user 10 QKQ1.190828.002 V12.0.2.0 QEJMIXM release-keys" \
